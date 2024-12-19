a6f44f4d1fc788e8517fced85cdfa1262ff5285d
DoomZyx <axelcella.ac@gmail.com>
2024-12-19 14:25:54 +0100

    Déploiement github pages

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/Kasa/',
   plugins: [react()],
   server: {
     open: true, // Ouvre automatiquement le projet dans le navigateur
