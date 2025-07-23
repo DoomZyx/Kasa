95ac9ede777faa93e5d2cc5762de07448725bc36
DoomZyx <axelcella.ac@gmail.com>
2025-07-24 00:59:20 +0200

    fix: remove GitHub Pages config for Vercel deployment"

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/Kasa/',
+  base: '/',
   plugins: [react()],
   server: {
     open: true, // Ouvre automatiquement le projet dans le navigateur
